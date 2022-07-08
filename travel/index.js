ab76fe92e9b19f5d1059af0152831fc4c6b153fd
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2022-07-08 16:42:07 +0300

    feat: add sign up pop up menu

--- travel/index.js
+++ travel/index.js
@@ -8,4 +8,55 @@ function closeNav() {
     document.getElementById("body-overlay").style.display = "none";
 }
 
-console.log('1. Верстка соответствует макету. Ширина экрана 390px +48\n2. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется не обрезается и не удаляется +0\n3. На ширине экрана 390px и меньше реализовано адаптивное меню +22\nОценка: 70 баллов') //вывести в консоль самооценку своей работы
+function openPopUpMenu() {
+    document.getElementById('login-menu').style.top = '100px';
+    document.getElementById("body-overlay").style.display = 'unset';
+}
+
+function closePopUpMenu() {
+    document.getElementById('login-menu').style.top = '-670px';
+    document.getElementById("body-overlay").style.display = "none"
+    document.getElementById('facebook').style.display = 'initial';
+    document.getElementById('google').style.display = 'initial';
+    document.getElementById('line-container').style.display = 'flex';
+    document.getElementById('popup-heading').textContent = 'Log in to your account';
+    document.getElementById('general').textContent = 'Sign In';
+    document.getElementById('forgot-password').style.display = 'initial';
+    document.getElementById('register1').style.display = 'initial';
+    document.getElementById('register2').style.display = 'none';
+    document.getElementById('login-menu').style.height = '660px';
+}
+
+function showAlert() {
+    let message = document.getElementById('login').value + ' ' + document.getElementById('password').value;
+    alert(message);
+}
+
+function changeMenu() {
+   document.getElementById('popup-heading').textContent = 'Create account';
+   document.getElementById('facebook').style.display = 'none';
+   /* document.getElementById('login').style.height = '54px'; */
+   /* document.getElementById('password').style.height = '54px'
+   document.getElementById('general').style.height = '54px'; */
+   document.getElementById('google').style.display = 'none';
+   document.getElementById('line-container').style.display = 'none';
+   document.getElementById('forgot-password').style.display = 'none';
+   document.getElementById('general').textContent = 'Sign Up';
+   document.getElementById('register1').style.display = 'none';
+   document.getElementById('register2').style.display = 'unset';
+   document.getElementById('login-menu').style.height = '436px';
+}
+
+function changeToLoginMenu() {
+    document.getElementById('facebook').style.display = 'initial';
+    document.getElementById('google').style.display = 'initial';
+    document.getElementById('line-container').style.display = 'flex';
+    document.getElementById('popup-heading').textContent = 'Log in to your account';
+    document.getElementById('general').textContent = 'Sign In';
+    document.getElementById('forgot-password').style.display = 'initial';
+    document.getElementById('register1').style.display = 'initial';
+    document.getElementById('register2').style.display = 'none';
+    document.getElementById('login-menu').style.height = '660px';
+}
+
+/* console.log('1. Верстка соответствует макету. Ширина экрана 390px +48\n2. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется не обрезается и не удаляется +0\n3. На ширине экрана 390px и меньше реализовано адаптивное меню +22\nОценка: 70 баллов') //вывести в консоль самооценку своей работы */
