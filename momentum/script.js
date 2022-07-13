063af9e3d96bbe48f6952cf02c79b22b96665995
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2022-07-13 22:52:22 +0300

    feat: add weather widget

--- momentum/script.js
+++ momentum/script.js
@@ -58,19 +58,20 @@ window.addEventListener('load', getLocalStorage)
 let randomNum;
 
 function getRandomNum(min, max) {
-    randomNum = Math.floor(Math.random() * (max - min + 1)) + 1;
-    return randomNum;
+    randomNum = (Math.floor(Math.random() * (max - min + 1)) + 1);
 }
+
+window.addEventListener('load', getRandomNum(1,20));
 
 function setBg() {
     const timeOfDayUpperCase = getTimeOfDay();
     const timeOfDay = timeOfDayUpperCase.toLowerCase();
-    const bgNum = getRandomNum(1, 20).toString().padStart(2, '0');
+    bgNum = randomNum.toString().padStart(2, '0');
     const img = new Image();
     
-    img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
+    img.src = `https://raw.githubusercontent.com/vberezhnykh/momentum-images/assets/images/${timeOfDay}/${bgNum}.jpg`;
     img.onload = () => {
-       document.body.style.backgroundImage = `url(https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg)`;
+       document.body.style.backgroundImage = `url(https://raw.githubusercontent.com/vberezhnykh/momentum-images/assets/images/${timeOfDay}/${bgNum}.jpg)`;
     };
 }
 
@@ -93,3 +94,28 @@ function getSlidePrev() {
 }
 
 document.querySelector('.previousSlide').addEventListener('click', getSlidePrev);
+
+const weatherIcon = document.querySelector('.icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.description');
+
+async function getWeather() {
+    const city = document.querySelector('.city');
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=eт&appid=fab720c19713c688933ff49eb63ac915&units=metric`
+    const res = await fetch (url);
+    const data = await res.json();
+    
+    weatherIcon.className = 'icon owf'; //придумать как выводить ошибку, если город не введен
+    /* if (city === undefined) {
+        weatherIcon.textContent = 'Error';
+    } else { */
+        weatherIcon.classList.add(`owf-${data.weather[0].id}`)
+        temperature.textContent = `${data.main.temp}°C`;
+        weatherDescription.textContent = data.weather[0].description;
+    /* }  */
+}
+
+addEventListener('load', getWeather)
+
+document.querySelector('.city').addEventListener('change', getWeather);
+
